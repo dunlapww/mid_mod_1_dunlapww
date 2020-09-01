e2de7f441e603e99e982062aa6f49f6c3b89d2d9
Will Dunlap <dunlapww@gmail.com>
2020-09-01 12:01:02 -0600

    Add vehicle class and tests

---
+++ lib/vehicle.rb
@@ -0,0 +1,27 @@
+class Vehicle
+  attr_reader :year, :make, :model, :passengers
+
+  def initialize(year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @speeding = false
+    @passengers = []
+  end
+
+  def speed
+    @speeding = true
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def num_adults?
+    @passengers.count{|psgr| psgr.adult?}
+  end
+end
